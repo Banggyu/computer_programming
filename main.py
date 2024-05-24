904b8bf0e2a5706531129a93602e68aae5030a14
Banggyu <163507549+Banggyu@users.noreply.github.com>
2024-05-24 16:48:48 +0900

    12주차 퀴즈
    붕어빵 누적 판매수에 따른 계산

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-def registration_number(identity_number):
-    if len(identity_number) != 14:
-        return False
-
-    if identity_number[6] != '-':
-        return False
-
-    identity_number = identity_number.replace('-', '')
-
-    weights = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5]
-    total = sum(int(identity_number[i]) * weights[i] for i in range(12))
-    remainder = (11 - total % 11) % 10
-
-    return remainder == int(identity_number[12])
+class Bun:
+    def __init__(self, custard_cream, price):
+        self.content = custard_cream
+        self.price = 1000
+        self.total = 0
 
 
-id_card_number = input("주민등록번호를 입력하십시오: ")
-if registration_number(id_card_number):
-    print("주민등록번호가 일치합니다.")
-else:
-    print("주민등록번호가 일치하지 않습니다.")
+    def sell(self):
+        self.total += self.price
+        print(self.content + "붕어빵이 판매되었습니다")
+
+custard_cream_bun = Bun("슈크림", 1000)
+custard_cream_bun.sell()
+custard_cream_bun.sell()
+
+print("누적 판매는" , custard_cream_bun.total , "입니다")
+
