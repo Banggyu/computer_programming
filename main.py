55cb393c79c3f290cfbfa18f2f7e7a75157f6aa4
Banggyu <163507549+Banggyu@users.noreply.github.com>
2024-06-07 15:12:53 +0900

    14주차 퀴즈
    키오스크 만들기2
    예외처리 ValueError, IndexError

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-def registration_number(identity_number):
-    if len(identity_number) != 14:
-        return False
+<<<<<<< HEAD
+menu = ["냉면", "볶음밥", "피자", "짜장면"]
 
-    if identity_number[6] != '-':
-        return False
+try:
+    choose = int(input("원하는 메뉴의 숫자를 입력하세요: "))
+    if choose < 1 or choose > 4:
+        raise IndexError
 
-    identity_number = identity_number.replace('-', '')
-
-    weights = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5]
-    total = sum(int(identity_number[i]) * weights[i] for i in range(12))
-    remainder = (11 - total % 11) % 10
-
-    return remainder == int(identity_number[12])
+    selected_menu = menu[choose - 1]
+    print((selected_menu) + "음식을 선택하셨습니다.")
 
 
-id_card_number = input("주민등록번호를 입력하십시오: ")
-if registration_number(id_card_number):
-    print("주민등록번호가 일치합니다.")
-else:
-    print("주민등록번호가 일치하지 않습니다.")
+except ValueError:
+    print("숫자를 입력하십시오. ")
+
+except IndexError:
+    print("1~4 사이의 숫자를 입력하십시오.")
+=======
+class Beverage:
+
+    def __init__(self, name, quantity):
+        self.menu = {"커피": 3000, "녹차": 2500, "아이스티": 3000}
+        self.name = name
+        self.quantity = quantity
+        self.total = 0
+
+    def calculate(self):
+        if self.name == "커피" or self.name == "녹차" or self.name == "아이스티":
+            self.total += self.menu[self.name]*int(self.quantity)
+            print(self.menu)
+        else:
+            print("메뉴를 잘못 입력하셨습니다.")
+
+
+user_input_1 = input("메뉴를 선택하세요")
+
+user_input_2 = input("수량을 입력하세요")
+order = Beverage(user_input_1, user_input_2)
+order.calculate()
+print(order.total)
+>>>>>>> 24011d1eaa535e2bdf88bffe9dbd54069772dcde
